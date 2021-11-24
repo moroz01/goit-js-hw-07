d6e58e55a4dde36ff73faa0f203bc7e9dbb25d48
moroz01 <aleksandr.moroz01@gmail.com>
2021-11-24 11:39:28 +0200

    Добавдяет ДЗ2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>`;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a");
+
+galleryContainer.addEventListener("click", onPictureClick);
+const pictures = document.querySelectorAll(".gallery__image");
+lightbox.options.captionsData = "alt";
+lightbox.options.captionType = "attr";
+lightbox.options.captionDelay = 250;
+
+function onPictureClick(evt) {
+  evt.preventDefault();
+}
+
